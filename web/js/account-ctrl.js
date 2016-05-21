d756274445aed8444a3ed7bf91706cb008124bcb
Agostino Manzi <info@agostinomanzi.it>
2016-05-21 13:05:17 +0200

    Begin of App
    Beginning of the AngularJS App dev.

---
+++ web/js/account-ctrl.js
@@ -0,0 +1,7 @@
+﻿var accountControllers = angular.module('accountControllers', []);
+
+accountControllers.controller('LoginCtrl', ['$scope', '$http', function ($scope, $http) {
+    $http.get('http://localhost/myboard/api/v10/account/login?usr=agos83@gmail.com&pwd=prova2').success(function (data) {
+        $scope.user = data;
+    });
+}]);
---
+++ web/js/app.js
@@ -0,0 +1,13 @@
+﻿var myboardApp = angular.module('myboardApp', ['ngRoute', 'accountControllers']);
+
+myboardApp.config(['$routeProvider',
+  function ($routeProvider) {
+      $routeProvider.
+        when('/login', {
+            templateUrl: 'partials/account/login.html',
+            controller: 'LoginCtrl'
+        }).
+        otherwise({
+            redirectTo: '/login'
+        });
+  }]);
